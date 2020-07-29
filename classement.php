788f3f204be98b9d8a71ac8eb2dce4fd15963116
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-07-29 16:21:07 +0200

    Create classement.php

---
+++ classement.php
@@ -0,0 +1,36 @@
+<div class="clear"></div>
+
+                <div class=" tricol-separation" id="showcase">
+                    <div class="tricol">
+                        <div class="head-skin-classement" data-player=""></div>
+
+                        <h3>Top #1 <b class="froz_color">FACTIONS</b></h3>
+                        <ul style="padding-top: 5px;">
+                            <h4><b style="color: #555555;">NOM : </b><b class="froz_color">Smooth</b></h4>
+                        </ul>
+                    </div>
+
+                    <div class="tricol second">
+                        <div class="head-skin-classement"></div>
+
+                        <h3>Top #1 <b class="froz_color">PVP</b></h3>
+                        <ul style="padding-top: 5px;">
+                            <h4><b style="color: #555555;">PSEUDO : </b><b class="color">AzgarTV</b></h4>
+                        </ul>
+                    </div>
+
+                    <div class="tricol">
+                        <div class="head-skin-classement" data-player="MelloW41"></div>
+
+                        <h3>Top #1 <b class="froz_color">VOTES</b></h3>
+                        <ul style="padding-top: 5px;">
+                            <h4><b style="color: #555555;">PSEUDO : </b><b class="color">Mello</b></h4>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clear"></div>
+                <hr>
+            </div>
+        </div>
+    </div>
+</div>
